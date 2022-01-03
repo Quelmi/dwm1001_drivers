041bcc1c2c3141a871dd15d3701151ae71d7dcb0
Quelmi <quelmi_serra@hotmail.com>
2022-01-03 16:02:31 +0100

    Changed folder where txt are saved

--- scripts/dwm1001_localization.py
+++ scripts/dwm1001_localization.py
@@ -215,23 +215,23 @@ class LocationEngine(object):
             z = self.optitrack_sub.pose.pose.position.z
             #gt_ranges = self.compute_ranges(np.array([x,y,z]), self.landmarks)
             now = rospy.get_rostime()
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_gt_pose.txt', np.array((x,y,z)))
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_time_stamps.txt', np.array([now.secs, now.nsecs]))
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_ranges.txt', np.array([ranges]))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_gt_pose.txt', np.array((x,y,z)))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_time_stamps.txt', np.array([now.secs, now.nsecs]))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_ranges.txt', np.array([ranges]))
             self.id +=1
             self.optitrack_in_world.publish(self.optitrack_sub.pose)
             self.optitrack_sub.new_pose = False
 
-        # save Optitrack AMCL if needed
+        # save AMCL reference if needed
         if self.odometry_sub.new_pose:
             x = self.odometry_sub.pose.pose.position.x
             y = self.odometry_sub.pose.pose.position.y
             z = self.odometry_sub.pose.pose.position.z
             #gt_ranges = self.compute_ranges(np.array([x,y,z]), self.landmarks)
             now = rospy.get_rostime()
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_gt_pose.txt', np.array((x,y,z)))
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_time_stamps.txt', np.array([now.secs, now.nsecs]))
-            #np.savetxt('/media/esau/hdd_at_ubuntu/bag_files/tmp/' + str(self.id) + '_ranges.txt', np.array([ranges]))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_gt_pose.txt', np.array((x,y,z)))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_time_stamps.txt', np.array([now.secs, now.nsecs]))
+            #np.savetxt('/home/miquelserra/localization/' + str(self.id) + '_ranges.txt', np.array([ranges]))
             self.id +=1
             self.optitrack_in_world.publish(self.odometry_sub.pose)
             self.odometry_sub.new_pose = False
